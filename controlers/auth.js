20b5b203437bf45b08dee16290350bc2e9afdde8
RMartires <rohitmartires14@gmail.com>
2020-05-05 23:42:33 +0530

    working with puppeteer

--- controlers/auth.js
+++ controlers/auth.js
@@ -1,84 +1,95 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
-const jwt = require('jsonwebtoken');
+const axios = require("axios");
+const jwt = require("jsonwebtoken");
+const puppeteer = require("puppeteer");
 
-
-var Airtable = require('airtable');
-var base = new Airtable({ apiKey: 'key6g32DRULc2ELR4' }).base('app0XNGZWSAZxUY6M');
-
+var Airtable = require("airtable");
+var base = new Airtable({ apiKey: "key6g32DRULc2ELR4" }).base(
+  "app0XNGZWSAZxUY6M"
+);
 
 exports.login = (req, res, next) => {
-    var username = req.params.username;
-    console.log(username);
+  var username = req.params.username;
+  console.log(username);
 
-    base('users').select({
-        filterByFormula: `{username} = '${username}'`,
-        view: "Grid view"
-    }).eachPage((records, fetchNextPage) => {
-
+  base("users")
+    .select({
+      filterByFormula: `{username} = '${username}'`,
+      view: "Grid view",
+    })
+    .eachPage(
+      (records, fetchNextPage) => {
         if (records.length === 1) {
-            //console.log(records[0].get('username'));
-            //console.log(records[0].get('profile_picture'));
-            //console.log(records[0].get('linkslu'));
-            const token = jwt.sign({
-                username: records[0].get('username'),
-                profile_picture: records[0].get('profile_picture'),
-                recordid: records[0].id
-            }, 'heyphil123');
+          //console.log(records[0].get('username'));
+          //console.log(records[0].get('profile_picture'));
+          //console.log(records[0].get('linkslu'));
+          const token = jwt.sign(
+            {
+              username: records[0].get("username"),
+              profile_picture: records[0].get("profile_picture"),
+              recordid: records[0].id,
+            },
+            "heyphil123"
+          );
 
-            res.json(
-                {
-                    token: token
-                }
-            );
-
+          res.json({
+            token: token,
+          });
         } else {
-            axios({
-                url: `https://www.instadp.com/fullsize/${username}`,
-                method: 'GET',
-                mode: 'cors'
-            })
-                .then(html => {
-                    var $ = cheerio.load(html.data);
-                    var data = $('.picture').attr();
-                    //console.log(data);
+          getprofilepicture(username).then((img) => {
+            console.log(img + " " + username);
+            base("users").create(
+              [
+                {
+                  fields: {
+                    username: username,
+                    profile_picture: img,
+                    links: [],
+                  },
+                },
+              ],
+              function (err, records) {
+                if (err) {
+                  console.error(err);
+                  return;
+                } else {
+                  const token = jwt.sign(
+                    {
+                      username: username,
+                      profile_picture: img,
+                      recordid: records[0].id,
+                    },
+                    "heyphil123"
+                  );
 
-                    base('users').create([
-                        {
-                            "fields": {
-                                "username": username,
-                                "profile_picture": data.src,
-                                "links": []
-                            }
-                        }
-                    ], function (err, records) {
-                        if (err) {
-                            console.error(err);
-                            return;
-                        } else {
-                            const token = jwt.sign({
-                                username: username,
-                                profile_picture: data.src,
-                                recordid: records[0].id
-                            }, 'heyphil123');
-
-                            res.json(
-                                {
-                                    token: token
-                                }
-                            );
-                        }
-                    });
-
-                })
-                .catch(err => {
-                    console.log(err);
-                });
+                  res.json({
+                    token: token,
+                  });
+                }
+              }
+            );
+          });
         }
-    }, (err) => {
-        if (err) { console.error(err); return; }
-    });
-
-
+      },
+      (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+      }
+    );
 };
 
+async function getprofilepicture(username) {
+  const puppeteer = require("puppeteer");
+
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto(`https://www.instadp.com/fullsize/${username}`, [
+    { waitUntil: "networkidle0" },
+  ]);
+  const img = await page.$eval(".picture", (el) => el.src);
+
+  //console.log(img);
+  page.close();
+  return img;
+}
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,9 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-var authcontroler = require('../controlers/auth');
+var authcontroler = require("../controlers/auth");
 
 /* GET home page. */
-router.get('/:username', authcontroler.login);
+router.get("/:username", authcontroler.login);
 
 module.exports = router;
